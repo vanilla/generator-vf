e089b302e9c125ab36a0152bf7a4812a11add5a1
beckyvb <beckyvanbussel@gmail.com>
2017-01-16 14:17:30 -0500

    Update array definitions and editor config.

--- app/templates/about.php
+++ app/templates/about.php
@@ -1,12 +1,12 @@
 <?php
 
-$<%= type %>Info['<%= directory %>'] = array(
-    'Name'        => "<%= name %>",<% if (desc) { %>
-    'Description' => "<%= desc %>",<% } %>
+$<%= type %>Info['<%= directory %>'] = [
+    'Name'        => '<%= name %>',<% if (desc) { %>
+    'Description' => '<%= desc %>',<% } %>
     'Version'     => '1.0.0',<% if (url) { %>
     'Url'         => '<%= url %>',<% } %>
-    'Author'      => "<%= author.name %>",<% if (author.email) { %>
+    'Author'      => '<%= author.name %>',<% if (author.email) { %>
     'AuthorEmail' => '<%= author.email %>',<% } if (author.url) { %>
     'AuthorUrl'   => '<%= author.url %>',<% } %>
     'License'     => '<%= license %>'
-);
+];
